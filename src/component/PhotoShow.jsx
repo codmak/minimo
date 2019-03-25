e9334fb6bfaa8db3e0b64abc2f38f4681b3316f6
aco <wwsxuan@163.com>
2019-03-25 16:53:12 +0800

    card => ball

--- src/component/PhotoShow.jsx
+++ src/component/PhotoShow.jsx
@@ -1,56 +1,122 @@
 import React, { PureComponent } from 'react';
+import { random } from '../util/';
+import { getColor } from '../componentUtil/loading/data/color';
 
 export default class PhotoShow extends PureComponent {
   constructor(props) {
     super(props);
+    const { width, height } = document.body.getBoundingClientRect();
+    console.log(width, height);
     this.stageRef = React.createRef();
     this.ballSplit = [12, 17, 21, 24, 27, 29, 30, 29, 27, 24, 21, 17, 12];
     this.state = {
-      cardInfo: new Array(290)
-        .fill(0)
-        .map((_, index) => ({ name: index, state: 0 })),
-      ballState: 0,
+      cardInfo: new Array(290).fill(0).map((_, index) => ({
+        name: index,
+        state: 0,
+        x: random(width - 300, 30),
+        y: random(height - 200, 20),
+        rotate: random(30, -30),
+        backgroundColor: getColor()
+      })),
+      // 0 - 初始状态
+      // 1 - 开始组成球形
+      // 2 - 球形组合完毕
+      // 3 - 卡片随意摆放
+      allCardState: 0,
       flag: true
     };
 
     // this.createBall();
+    // this.initBall();
+    // this.cardShow();
+
+    Promise.resolve()
+      .then(this.cardShow)
+      .then(this.initBall)
+      .then(this.createBall);
   }
 
-  createBall = () => {
-    const { cardInfo } = this.state;
-    let index = 0;
-    let timer = setInterval(() => {
-      if (index === cardInfo.length) {
-        clearInterval(timer);
-        this.setState({
-          ballState: 1
-        });
-        return;
-      }
-      cardInfo[index].state = 1;
-      this.setState(state => ({
-        flag: !state.flag
-      }));
-      index++;
-    }, 200);
-  };
+  createBall = () =>
+    new Promise(resolve => {
+      const { cardInfo } = this.state;
+      // eslint-disable-next-line
+      this.state.allCardState = 1;
+      let index = 0;
+      let timer = setInterval(() => {
+        if (index === cardInfo.length) {
+          clearInterval(timer);
+          this.setState({
+            allCardState: 2
+          });
+          resolve();
+          return;
+        }
+        cardInfo[index].state = 2;
+        this.setState(state => ({
+          flag: !state.flag
+        }));
+        index++;
+      }, 200);
+    });
+
+  initBall = () =>
+    new Promise(resolve => {
+      const { cardInfo } = this.state;
+      let index = cardInfo.length - 1;
+      let timer = setInterval(() => {
+        if (index === -1) {
+          clearInterval(timer);
+          resolve();
+          return;
+        }
+        cardInfo[index].state = 1;
+        this.setState(state => ({
+          flag: !state.flag
+        }));
+        index--;
+      }, 20);
+    });
+
+  cardShow = () =>
+    new Promise(resolve => {
+      const { cardInfo } = this.state;
+      let index = 0;
+      let timer = setInterval(() => {
+        if (index === cardInfo.length) {
+          clearInterval(timer);
+          resolve();
+          return;
+        }
+        cardInfo[index].state = 3;
+        this.setState(state => ({
+          flag: !state.flag
+        }));
+        index++;
+      }, 200);
+    });
 
   getCardStateClass = cardState => {
     switch (cardState) {
       case 0:
-        return 'flat';
+        return 'init';
       case 1:
+        return 'circle-before';
+      case 2:
         return 'circle';
+      case 3:
+        return 'flat';
       default:
         return '';
     }
   };
 
-  getBallStateClass = ballState => {
-    switch (ballState) {
+  getBallStateClass = allCardState => {
+    switch (allCardState) {
       case 0:
         return '';
       case 1:
+        return 'out-rotate';
+      case 2:
         return 'out-rotate';
       default:
         return '';
@@ -80,22 +146,21 @@ export default class PhotoShow extends PureComponent {
       stage.style.animationName = 'none';
       setTimeout(() => {
         stage.style.transform = initState;
-        stage.addEventListener('transitionend', function listen() {
+        setTimeout(() => {
           stage.style.transform = lateState;
-          stage.removeEventListener('transitionend', listen);
           stage.addEventListener('transitionend', function listen2() {
             stage.style.animationName = animationName;
             stage.style.animationDuration = animationDuration;
             stage.style.animationPlayState = 'running';
             stage.removeEventListener('transitionend', listen2);
           });
-        });
+        }, 1000);
       });
     }, 500);
   };
 
   render() {
-    const { cardInfo } = this.state;
+    const { cardInfo, allCardState } = this.state;
     return (
       <div className="photo-wrap-all">
         <span
@@ -117,12 +182,27 @@ export default class PhotoShow extends PureComponent {
           out
         </span>
         <div className="p-r pwa-stage">
-          <div className={`p-a pwa-view`} ref={this.stageRef}>
+          <div
+            className={`p-a pwa-view ${this.getBallStateClass(allCardState)}`}
+            ref={this.stageRef}
+          >
             {cardInfo.map((item, index) => (
               <div
                 className={`pwa-cel ${this.getCardStateClass(
                   item.state
                 )} cel-${index}`}
+                style={
+                  item.state === 3
+                    ? {
+                        top: item.y,
+                        left: item.x,
+                        transform: `rotate(${item.rotate}deg)`,
+                        backgroundColor: item.backgroundColor
+                      }
+                    : {
+                        backgroundColor: item.backgroundColor
+                      }
+                }
                 key={index}
               />
             ))}
